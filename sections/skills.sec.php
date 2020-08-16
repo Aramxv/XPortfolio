8427990c2f985754dd31adc7477d27d0d0c1ebd4
Aramxv <reijraxyr@gmail.com>
2020-08-16 23:56:38 +0800

    Changed the style of About me and Skills Section

---
+++ sections/skills.sec.php
@@ -0,0 +1,17 @@
+<div class="main-skill"><h4>Top Expertise</h4></div>
+<div class="skills-container1">
+    <div><img src="assets/lang/icons8-html-5-240.png" alt="HTML5"></div>
+    <div><img src="assets/lang/icons8-css3-240.png" alt="CSS3"></div>
+    <div><img src="assets/lang/icons8-javascript-240.png" alt="JavaScript"></div>
+    <div><img src="assets/lang/icons8-react-native-240.png" alt="React.js"></div>
+    <div><img src="assets/lang/icons8-bootstrap-240.png" alt="Bootstrap"></div>
+    <div><img src="assets/lang/icons8-sass-240.png" alt="SASS"></div>
+</div>
+<div class="skills-container2">
+    <div><img src="assets/lang/icons8-heroku-256.png" alt="HTML5"></div>
+    <div><img src="assets/lang/icons8-microsoft-sql-server-240.png" alt="CSS3"></div>
+    <div><img src="assets/lang/c-sharp.png" alt="JavaScript"></div>
+    <div><img src="assets/lang/php.png" alt="React.js"></div>
+    <div><img src="assets/lang/icons8-laravel-96.png" alt="Bootstrap"></div>
+    <div><img src="assets/lang/nextpng.png" alt="SASS"></div>
+</div>
